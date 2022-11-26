f6c065ab0642f05e0f3b93f10dd0edc9d882df83
izv <barzakouski.sergey@gmail.com>
2022-11-26 22:17:03 +0300

    html code

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        dots: true,
+        prevArrow: "<button type=\"button\" class=\"slick-prev\"><img src='../assets/image/carousel/left.jpg' alt='left'></button>",
+        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='../assets/image/carousel/chevron-right-solid.jpg' alt='right'></button>",
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        cssEase: "linear",
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows:false,
+                    dots: true
+                }
+            },
+        ]
+
+
+    });
+});
