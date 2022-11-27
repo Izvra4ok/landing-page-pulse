e3623786ce6861bd72dc6c081708533ee29aec4a
izv <barzakouski.sergey@gmail.com>
2022-11-27 20:37:08 +0300

    html code + carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $('.carousel__inner').slick({
         dots: true,
-        prevArrow: "<button type=\"button\" class=\"slick-prev\"><img src='../assets/image/carousel/left.jpg' alt='left'></button>",
-        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='../assets/image/carousel/chevron-right-solid.jpg' alt='right'></button>",
+        // prevArrow: "<button type=\"button\" class=\"slick-prev\"><img src='../assets/image/carousel/left.jpg' alt='left'></button>",
+        // nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='../assets/image/carousel/chevron-right-solid.jpg' alt='right'></button>",
+        prevArrow: "<button type=\"button\" class=\"slick-prev\"><img src='../src/assets/image/carousel/left.jpg' alt='left'></button>",
+        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='.../src/assets/image/carousel/chevron-right-solid.jpg' alt='right'></button>",
         infinite: true,
         speed: 500,
         autoplay: true,
@@ -22,3 +24,5 @@ $(document).ready(function () {
 
     });
 });
+
+
