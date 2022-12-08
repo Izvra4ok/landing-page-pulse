b5f4ec7728dca41ad695b1b1af8ec61e60818a5e
unknown <barzakouski.sergey@gmail.com>
2022-12-08 13:49:19 +0300

    gulp ipgrade

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'reacttestingapp@gmail.com';                 // Наш логин
-$mail->Password = 'gutlpqnmrxeuimtz';                           // Наш пароль от ящика
+$mail->Username = 'example@gmail.com';                 // Наш логин
+$mail->Password = 'examplepassword';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('reacttestingapp@gmail.com', 'Pulse');   // От кого письмо
+$mail->setFrom('example@gmail.com', 'Pulse');   // От кого письмо
 $mail->addAddress('barzakouski.sergey@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
